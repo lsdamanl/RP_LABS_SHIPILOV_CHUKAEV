da4c9a3782cf8594e58e08f961d5340ab53d5fd5
lsdamanl <shipiloffdima@ya.ru>
2025-04-04 20:27:17 +0700

    Task_2_2 changed

--- Laboratornaya2/lab_2_2.py
+++ Laboratornaya2/lab_2_2.py
@@ -4,8 +4,8 @@
 # 3. Вывести в консоль количество замен
 
 input_string = input("Введите строку: ")
+replace_count = input_string.count(":")
 modified_string = input_string.replace(":", "%")
-replace_count = len(input_string) - len(modified_string.replace("%", ":"))
 
 print("Модифицированная строка:", modified_string)
 print("Количество замен:", replace_count)
